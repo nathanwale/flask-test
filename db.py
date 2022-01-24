e2feeb0648a5abfb77cae8c891d44b52f3c4333f
nathanwale <48480798+nathanwale@users.noreply.github.com>
2022-01-24 13:30:16 +1100

    Convert db url scheme to 'postgresql' if it is 'postgres'
    'postgres' isn't supported by SQLAlchemy!
    The db url on Heroku is 'postgres'!

--- db.py
+++ db.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine, text
+from urllib.parse import urlparse, urlunparse
 import os
 
-database_url = os.environ.get('DATABASE_URL', 'postgresql://localhost/flask_test')
-engine = create_engine(database_url, echo=True, future=True)
+def init_engine():
+    db_url = os.environ.get('DATABASE_URL', 'postgresql://localhost/flask_test')
+    parsed_url = urlparse(db_url)
+    if parsed_url.scheme == 'postgres':
+        db_url = urlunparse(('postgresql', *parsed_url[1:]))
+    print(f"connecting to databse at {db_url}")
+    return create_engine(db_url, echo=True, future=True)
+    
+engine = init_engine()
 
 #
 # Run a DB query
